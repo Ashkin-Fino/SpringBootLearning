da3dae311115f0f4076340c1578d09bf38817903
Ashkin Fino <rashkinfino421@gmail.com>
2026-05-24 21:21:43 +0530

    Added JwtAuthenticationFilter class. Added in security config.

--- src/main/java/com/airtribe/learning/config/SecurityConfig.java
+++ src/main/java/com/airtribe/learning/config/SecurityConfig.java
@@ -1,20 +1,37 @@
 package com.airtribe.learning.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.airtribe.learning.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .sessionManagement(session -> session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+            ))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated())
+            .addFilterBefore(
+                jwtAuthenticationFilter, 
+                UsernamePasswordAuthenticationFilter.class
+            );
         return http.build();
     }
 
